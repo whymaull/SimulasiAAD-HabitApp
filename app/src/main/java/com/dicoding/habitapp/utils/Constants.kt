48a6d99c14f8dcc95a7a63349fada28a9ada080f
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-12-09 02:39:01 +0700

    Update Code

--- app/src/main/java/com/dicoding/habitapp/utils/Constants.kt
+++ app/src/main/java/com/dicoding/habitapp/utils/Constants.kt
@@ -4,6 +4,5 @@ const val HABIT = "HABIT"
 const val HABIT_ID = "HABIT_ID"
 const val HABIT_TITLE = "HABIT_TITLE"
 const val NOTIFICATION_CHANNEL_ID = "notify-channel"
-const val NOTIFICATION_CHANNEL_ID_NUMBER = 101
 const val NOTIF_UNIQUE_WORK: String = "NOTIF_UNIQUE_WORK"
 
