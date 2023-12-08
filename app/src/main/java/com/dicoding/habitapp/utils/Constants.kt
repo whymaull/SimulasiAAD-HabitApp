1b5be76dcaa4d4d2a4294ac20764e474363ccc03
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-12-09 02:01:14 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/habitapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.dicoding.habitapp.utils
+
+const val HABIT = "HABIT"
+const val HABIT_ID = "HABIT_ID"
+const val HABIT_TITLE = "HABIT_TITLE"
+const val NOTIFICATION_CHANNEL_ID = "notify-channel"
+const val NOTIFICATION_CHANNEL_ID_NUMBER = 101
+const val NOTIF_UNIQUE_WORK: String = "NOTIF_UNIQUE_WORK"
+
